6fc88f957bbed550b2e35cdf3e83fcfa500f5d03
lucassouzapb <lycasoliveira@gmail.com>
2022-06-29 08:50:10 -0300

    Atualização

--- backend/target/classes/admin.sql
+++ backend/target/classes/admin.sql
@@ -1,6 +1,9 @@
 -- senha 123456a $2a$10$kqjrSqjDd8MQXIVzlxu/NeJU5N6GKDJ4v3ZFesBKj54N4o7gnPWEm
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO public.users(id, email, password, username, role_id)
-VALUES (nextval('users_id_seq'), 'admin@email.com', '$2a$10$kqjrSqjDd8MQXIVzlxu/NeJU5N6GKDJ4v3ZFesBKj54N4o7gnPWEm', 'admin', 1);
+VALUES (nextval('users_id_seq'), 'admin@email.com', '$2a$10$kqjrSqjDd8MQXIVzlxu/NeJU5N6GKDJ4v3ZFesBKj54N4o7gnPWEm', 'administrador', 1);
 
 
 
